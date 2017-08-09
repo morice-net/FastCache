d62441f7229262e2dfdaa9cafb37a7a6ed3f5f53
artaud <jeannicolasartaud@gmail.com>
2017-08-09 07:31:04 +0200

    Add the WebEngine to the pro file
    Iniatialize the webEngine in the maon, after adding t in the pro file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QGuiApplication>
+#include <QtWebEngine/qtwebengineglobal.h>
 #include <QQuickView>
 #include <QQuickItem>
 #include "connector.h"
@@ -8,8 +9,10 @@ int main(int argc, char *argv[])
     qmlRegisterType<Connector>("com.mycompany.connecting", 1, 0, "Connector");
 
     QGuiApplication app(argc, argv);
+    QtWebEngine::initialize();
 
     QQuickView view(QUrl(QStringLiteral("qrc:/main.qml")));
+
     view.setWidth(450);
     view.setHeight(840);
     view.show();
