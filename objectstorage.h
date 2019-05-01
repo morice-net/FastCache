eb1b1301d2b46be5db063077a25c9749ac2ebfa8
Jean-Nicolas Artaud <jeannicolasartaud@gmail.com>
2019-05-01 10:12:01 +0200

    Indent the object storage header

--- objectstorage.h
+++ objectstorage.h
@@ -7,12 +7,12 @@
 
 class ObjectStorage : public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	explicit ObjectStorage(QObject *parent = 0);
+    explicit ObjectStorage(QObject *parent = 0);
 
-	virtual bool createObject(QString tableName, QVector<QString> columnNames, QVector<QString> columnValues) = 0;
+    virtual bool createObject(QString tableName, QVector<QString> columnNames, QVector<QString> columnValues) = 0;
     virtual bool createTable(QString tableName, QVector<QString> columnNames, QVector<QString> columnTypes) = 0;
     virtual QString stringFromType(QVariant::Type type) const = 0;
 
@@ -20,19 +20,19 @@ public:
     bool createTableFromObject(QObject* dataRow);
     QString serializeValue(const QVariant &variant) const;
 
-	QVector<QObject *> dataTableObjects(const QString& tableName) const;
-	QVector<QObject *> dataObjects() const;
+    QVector<QObject *> dataTableObjects(const QString& tableName) const;
+    QVector<QObject *> dataObjects() const;
     QVector<QString> tableNames() const;
 
 protected:
-	/**
-	 * List of tables
-	 * Table name is the object name of the QObject
-	 * Columns are Q_PROPERTIES
-	 * Rows are children objects (one object is one row)
-	 */
-	QVector<QObject*> m_dataObjects;
-	QVector<QString> m_tableNames;
+    /**
+     * List of tables
+     * Table name is the object name of the QObject
+     * Columns are Q_PROPERTIES
+     * Rows are children objects (one object is one row)
+     */
+    QVector<QObject*> m_dataObjects;
+    QVector<QString> m_tableNames;
 
 };
 
