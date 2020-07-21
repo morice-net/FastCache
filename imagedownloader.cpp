70445f581131495b0d551034b4ed77ccfd57a77d
morice-net <artaud.g00@free.fr>
2020-07-21 17:22:32 +0200

    Complete files imagedownloader.h,cpp.

--- imagedownloader.cpp
+++ imagedownloader.cpp
@@ -1,10 +1,68 @@
 #include "imagedownloader.h"
+#include <QDebug>
 
 ImageDownloader::ImageDownloader(QObject *parent)
-    : QObject(parent)
+    : QObject(parent),
+      webCtrl(new QNetworkAccessManager(this))
 {
 }
 
 ImageDownloader::~ImageDownloader()
 {
+    delete webCtrl;
+}
+
+void ImageDownloader::downloadImage(QUrl url, QString id, QString path)
+{
+    QString url_string = url.toString();
+
+    QFile *file = new QFile(path, this);
+    if(!file->open(QIODevice::WriteOnly))
+    {
+        return;
+    }
+
+    QNetworkRequest request(url);
+    request.setRawHeader("User-Agent", userAgent);
+
+    QNetworkReply *reply = webCtrl->get(request);
+    replytofile.insert(reply, file);
+    replytopathid.insert(reply, QPair<QString, QString>(path, id));
+
+    QObject::connect(reply, &QNetworkReply::finished, this, &ImageDownloader::imageDownloaded);
+    QObject::connect(reply, &QNetworkReply::readyRead, this, &ImageDownloader::onReadyRead);
+}
+
+void ImageDownloader::imageDownloaded()
+{
+    QNetworkReply* reply = qobject_cast<QNetworkReply*>(sender());
+
+    if (replytofile[reply]->isOpen())
+    {
+        replytofile[reply]->close();
+        replytofile[reply]->deleteLater();
+    }
+
+    switch(reply->error())
+    {
+    case QNetworkReply::NoError:
+        break;
+
+    default:
+        emit error(reply->errorString().toLatin1());
+        break;
+    }
+
+    emit downloaded(replytopathid[reply].first, replytopathid[reply].second);
+
+    replytofile.remove(reply);
+    replytopathid.remove(reply);
+    delete reply;
+}
+
+void ImageDownloader::onReadyRead()
+{
+    QNetworkReply* reply = qobject_cast<QNetworkReply*>(sender());
+
+    replytofile[reply]->write(reply->readAll());
 }
--- imagedownloader.h
+++ imagedownloader.h
@@ -2,14 +2,39 @@
 #define IMAGEDOWNLOADER_H
 
 #include <QObject>
+#include <QStringList>
+#include <QFile>
+#include <QDir>
+
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
 
 class ImageDownloader : public QObject
-{    
+{
     Q_OBJECT
 
 public:
     explicit ImageDownloader(QObject *parent = nullptr);
-    ~ImageDownloader() ;
+    virtual~ImageDownloader() ;
+    void downloadImage(QUrl url, QString id, QString dir_absolute_path);
+
+signals:
+    // emits error string
+    void error(QString);
+    // Emits path to img on disk and id
+    void downloaded(QString, QString);
+
+private slots:
+    void imageDownloaded();
+    void onReadyRead();
+
+private:
+    QNetworkAccessManager *webCtrl;
+    QMap<QNetworkReply*, QFile*> replytofile;
+    QMap<QNetworkReply*, QPair<QString, QString> > replytopathid;
+
+    const QByteArray userAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36";
 };
 
 #endif // IMAGEDOWNLOADER_H
